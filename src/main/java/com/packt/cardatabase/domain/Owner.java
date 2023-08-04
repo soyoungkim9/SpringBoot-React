e3e38279222b40d1f9fdd0ded51b067014ab91e0
soyoungkim9 <soyoung930623@gmail.com>
2023-08-04 23:10:11 +0900

    springBoot 기초

---
+++ src/main/java/com/packt/cardatabase/domain/Owner.java
@@ -0,0 +1,56 @@
+package com.packt.cardatabase.domain;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Owner {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long ownerId;
+  private String firstname, lastname;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+  private List<Car> cars;
+
+  public Owner() {}
+
+  public Owner(String firstname, String lastname) {
+    super();
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+
+  public long getOwnerId() {
+    return ownerId;
+  }
+
+  public void setOwnerId(long ownerId) {
+    this.ownerId = ownerId;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public List<Car> getCars() {
+    return cars;
+  }
+
+  public void setCars(List<Car> cars) {
+    this.cars = cars;
+  }
+}
