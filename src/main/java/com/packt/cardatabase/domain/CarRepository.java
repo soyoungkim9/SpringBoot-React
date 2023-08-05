096546385c664625e9d9a0d1ccb79b3319ec7ea3
soyoungkim9 <soyoung930623@gmail.com>
2023-08-05 23:40:05 +0900

    RESTful API

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,5 +1,13 @@
 package com.packt.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface CarRepository extends CrudRepository<Car, Long> {}
+import java.util.List;
+
+@RepositoryRestResource
+public interface CarRepository extends CrudRepository<Car, Long> {
+  List<Car> findByBrand(@Param("brand") String brand);
+  List<Car> findByColor(@Param("color") String color);
+}
--- src/main/java/com/packt/cardatabase/domain/Owner.java
+++ src/main/java/com/packt/cardatabase/domain/Owner.java
@@ -1,16 +1,20 @@
 package com.packt.cardatabase.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
 import java.util.List;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Owner {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long ownerId;
   private String firstname, lastname;
 
+  @JsonIgnore
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
   private List<Car> cars;
 
