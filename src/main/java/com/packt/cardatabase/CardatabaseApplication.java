832c240713fdbe6bfe09ec059cc87b8e4550b607
soyoungkim9 <soyoung930623@gmail.com>
2023-08-07 00:02:27 +0900

    스프링시큐리티 jwt방식 적용

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,9 +1,6 @@
 package com.packt.cardatabase;
 
-import com.packt.cardatabase.domain.Car;
-import com.packt.cardatabase.domain.CarRepository;
-import com.packt.cardatabase.domain.Owner;
-import com.packt.cardatabase.domain.OwnerRepository;
+import com.packt.cardatabase.domain.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +14,8 @@ import java.util.Arrays;
 public class CardatabaseApplication implements CommandLineRunner {
 	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
+	@Autowired
+	private UserRepository urepository;
 	@Autowired
 	private CarRepository repository;
 
@@ -33,12 +32,17 @@ public class CardatabaseApplication implements CommandLineRunner {
 		Owner owner2 = new Owner("Mary", "Robinsom");
 		orepository.saveAll(Arrays.asList(owner1, owner2));
 
-		repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2021, 59000, owner1));
-		repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2019, 29000, owner2));
-		repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2020, 39000, owner2));
+		Car car1 = new Car("Ford", "Mustang", "Red", "ADF-1121", 2021, 59000, owner1);
+		Car car2 = new Car("Nissan", "Leaf", "White", "SSJ-3002", 2019, 29000, owner2);
+		Car car3 = new Car("Toyota", "Prius", "Silver", "KKO-0212", 2020, 39000, owner2);
+		repository.saveAll(Arrays.asList(car1, car2, car3));
 
 		for(Car car : repository.findAll()) {
 			logger.info(car.getBrand() + " " + car.getModel());
 		}
+
+		urepository.save(new User("user", "$2y$10$dX8bUJ7LkhT4TS94u97cVu2zeuCWndHHvS2qgmzFMMfRGVcNOoObm", "USER"));
+		urepository.save(new User("admin", "$2y$10$OVJ5vA2l3vb.Eqb.llUNGu3W.OWQFLkTTKkGz/Og9HJOt4stMHYBC", "ADMIN"));
+
 	}
 }
---
+++ src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.packt.cardatabase;
+
+import com.packt.cardatabase.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Autowired
+  private UserDetailsServiceImpl userDetailsService;
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService)
+      .passwordEncoder(new BCryptPasswordEncoder());
+  }
+
+  @Bean
+  public AuthenticationManager getAuthenticationManager() throws Exception {
+    return authenticationManager();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+      .sessionManagement()
+      .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+      .authorizeRequests()
+      // /login 엔드포인트에 대한 POST요청은 보호되지 않음
+      .antMatchers(HttpMethod.POST, "/login").permitAll()
+      // 다른 모든 요청은 보호됨
+      .anyRequest().authenticated();
+  }
+}
