efc5d77901cbff69e259d23f43ae5e3e2c22d411
soyoungkim9 <soyoung930623@gmail.com>
2023-08-07 22:19:01 +0900

    cors 필터 추가

--- src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -37,9 +42,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     return authenticationManager();
   }
 
+  // CORS 필터 추가
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(Arrays.asList("*"));
+    config.setAllowedMethods(Arrays.asList("*"));
+    config.setAllowedHeaders(Arrays.asList("*"));
+    config.setAllowCredentials(false);
+    config.applyPermitDefaultValues();
+
+    source.registerCorsConfiguration("/**", config);
+    return source;
+  }
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable()
+    http.csrf().disable().cors().and()
       .sessionManagement()
       .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
       .authorizeRequests()
